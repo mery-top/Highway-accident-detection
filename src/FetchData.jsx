fb3ec16f43e255a71cf98541787f30c7e2a1e7f8
Meerthika <meerthikasrrajendrans@gmail.com>
2025-02-09 10:28:54 +0530

    Revamp FetchData

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -2,24 +2,10 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function FetchData() {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-      axios.get("http://127.0.0.1:8000/")
-        .then((response) => setData(response.data))
-        .catch((error) => console.error("Error fetching data:", error));
-    }, []);
-  
-    return (
-      <div>
-        <h1>FastAPI + React</h1>
-        {data ? <p>{data.message}</p> : <p>Loading...</p>}
-      </div>
-    );
-  };
     
   return (
     <div>FetchData</div>
   )
+}
 
 export default FetchData
